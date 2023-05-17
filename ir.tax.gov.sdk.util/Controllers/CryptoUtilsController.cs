e75df26b99ce2ec31f09fed5eb2247ab68e6c9c8
ali <alikhodaveerdi@yahoo.com>
2023-05-17 16:06:47 +0330

    Add project files.

---
+++ ir.tax.gov.sdk.util/Controllers/CryptoUtilsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ir.tax.gov.sdk.util.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoUtilsController : ControllerBase
+    {
+        public static CryptoUtils cryptoUtils =new CryptoUtils();
+
+        [HttpPost("/test")]
+        public ActionResult<CryptoUtils> Test()
+        {
+            var testc =cryptoUtils.GetType();
+            return Ok(testc);
+        }
+
+
+    }
+}
